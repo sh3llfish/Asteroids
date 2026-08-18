bbda127456da9bb445779dd2f630f9def49dffe4
sh3llfish <coryritsuko@163.com>
2026-08-19 02:20:26 +1000

    feat: player movement

--- player.py
+++ player.py
@@ -1,7 +1,6 @@
-from locale import D_T_FMT
 from typing import override
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 
@@ -33,5 +32,14 @@ class Player(CircleShape):
             self.rotate(-1 * dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-1 * dt)
         print(self.rotation)
+
+    def move(self, dt: float):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
